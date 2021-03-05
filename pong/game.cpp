65b55ba3099500198e3c0beb574d966df478b44f
Samuel <samcashlamore@gmail.com>
2021-03-05 17:25:52 -0500

    game works time for cleaning

--- pong/game.cpp
+++ pong/game.cpp
@@ -3,22 +3,22 @@
 #define released(b) (!input->buttons[b].is_down && input->buttons[b].changed)
 
 float player_1_p, player_1_dp, player_2_p, player_2_dp;
-float arena_half_size_x = 85, arearena_half_size_y = 45;
+float arena_half_size_x = 85, arena_half_size_y = 45;
 float player_half_size_x = 2.5, player_half_size_y = 12;
 float ball_p_x, ball_p_y, ball_dp_x = 130, ball_dp_y, ball_half_size = 1;
 
 internal void
 simulate_game(Input* input, float dt) {
 	clear_screen(16777215);
-	draw_rect(0, 0, arena_half_size_x, arearena_half_size_y, 0);
+	draw_rect(0, 0, arena_half_size_x, arena_half_size_y, 0);
 
 	float player_1_ddp = 0.f;
-	if (is_down(BUTTON_UP)) player_1_ddp += 1000;
-	if (is_down(BUTTON_DOWN)) player_1_ddp -= 1000;
+	if (is_down(BUTTON_UP)) player_1_ddp += 1200;
+	if (is_down(BUTTON_DOWN)) player_1_ddp -= 1200;
 
 	float player_2_ddp = 0.f;
-	if (is_down(BUTTON_W)) player_2_ddp += 1000;
-	if (is_down(BUTTON_S)) player_2_ddp -= 1000;
+	if (is_down(BUTTON_W)) player_2_ddp += 1200;
+	if (is_down(BUTTON_S)) player_2_ddp -= 1200;
 
 	player_1_ddp -= player_1_dp * 10.f;
 
@@ -26,12 +26,12 @@ simulate_game(Input* input, float dt) {
 	player_1_p = player_1_p + player_1_dp * dt + player_1_ddp * dt * dt * 0.5f;
 	player_1_dp = player_1_dp + player_1_ddp * dt;
 
-	if (player_1_p + player_half_size_y > arearena_half_size_y) {
-		player_1_p = arearena_half_size_y - player_half_size_y;
+	if (player_1_p + player_half_size_y > arena_half_size_y) {
+		player_1_p = arena_half_size_y - player_half_size_y;
 		player_1_dp *= -0.5;
 	}
-	else if (player_1_p - player_half_size_y < -arearena_half_size_y) {
-		player_1_p = -arearena_half_size_y + player_half_size_y;
+	else if (player_1_p - player_half_size_y < -arena_half_size_y) {
+		player_1_p = -arena_half_size_y + player_half_size_y;
 		player_1_dp *= -0.5;
 	}
 
@@ -40,17 +40,57 @@ simulate_game(Input* input, float dt) {
 	player_2_p = player_2_p + player_2_dp * dt + player_2_ddp * dt * dt * 0.5f;
 	player_2_dp = player_2_dp + player_2_ddp * dt;
 
-	if (player_2_p + player_half_size_y > arearena_half_size_y) {
-		player_2_p = arearena_half_size_y - player_half_size_y;
+	if (player_2_p + player_half_size_y > arena_half_size_y) {
+		player_2_p = arena_half_size_y - player_half_size_y;
 		player_2_dp *= -0.5;
 	}
-	else if (player_2_p - player_half_size_y < -arearena_half_size_y) {
-		player_2_p = -arearena_half_size_y + player_half_size_y;
+	else if (player_2_p - player_half_size_y < -arena_half_size_y) {
+		player_2_p = -arena_half_size_y + player_half_size_y;
 		player_2_dp *= -0.5;
 	}
 
-	draw_rect(0, 0, 1, 1, 16777215);
+	ball_p_x += ball_dp_x * dt;
+	ball_p_y += ball_dp_y * dt;
 
+	draw_rect(ball_p_x, ball_p_y, 1, 1, 16777215);
+
+	if (ball_p_x + ball_half_size > 80 - player_half_size_x &&
+		ball_p_x - ball_half_size < 80 + player_half_size_x &&
+		ball_p_y + ball_half_size > player_1_p - player_half_size_y &&
+		ball_p_y - ball_half_size < player_1_p + player_half_size_y) {
+		ball_p_x = 80 - player_half_size_x - ball_half_size;
+		ball_dp_x *= -1;
+		ball_dp_y = (ball_p_y - player_1_p)*2 + player_1_dp * 0.5;
+	}
+	else if (ball_p_x + ball_half_size > -80 - player_half_size_x &&
+		ball_p_x - ball_half_size < -80 + player_half_size_x &&
+		ball_p_y + ball_half_size > player_2_p - player_half_size_y &&
+		ball_p_y - ball_half_size < player_2_p + player_half_size_y) {
+		ball_p_x = -80 + player_half_size_x + ball_half_size;
+		ball_dp_x *= -1;
+		ball_dp_y = (ball_p_y - player_2_p) * 2 + player_2_dp * 0.5;
+	}
+
+	if (ball_p_y + ball_half_size > arena_half_size_y) {
+		ball_p_y = arena_half_size_y - ball_half_size;
+		ball_dp_y *= -1;
+	}
+	else if (ball_p_y - ball_half_size < -arena_half_size_y) {
+		ball_p_y = -arena_half_size_y + ball_half_size;
+		ball_dp_y *= -1;
+	}
+	if (ball_p_x + ball_half_size > arena_half_size_x) {
+		ball_p_y = 0;
+		ball_p_x = 0;
+		ball_dp_y = 0;
+		ball_dp_x *= -1;
+	}
+	else if (ball_p_x - ball_half_size < -arena_half_size_x) {
+		ball_p_x = 0;
+		ball_p_x = 0;
+		ball_dp_y = 0;
+		ball_dp_x *= -1;
+	}
 	draw_rect(80, player_1_p, 2.5, 12, 16777215);
 	draw_rect(-80, player_2_p, 2.5, 12, 16777215);
 }
